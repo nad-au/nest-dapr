2b3a30a6e672ce92046c448beda183b531650677
Neil Dobson <neil.dobson71@gmail.com>
2022-07-02 17:10:34 +1000

    Build

---
+++ lib/index.ts
@@ -0,0 +1,5 @@
+import { DaprBinding } from './dapr-binding.decorator';
+import { DaprPubSub } from './dapr-pubsub.decorator';
+import { DaprModule } from './dapr.module';
+
+export { DaprBinding, DaprPubSub, DaprModule };
