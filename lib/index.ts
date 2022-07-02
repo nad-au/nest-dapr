ca4eccea85f60cf413539fdaed43c0e12328c888
Neil Dobson <neil.dobson71@gmail.com>
2022-07-02 17:43:11 +1000

    Build

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,18 @@
-import { DaprBinding } from './dapr-binding.decorator';
-import { DaprPubSub } from './dapr-pubsub.decorator';
+import { DAPR_BINDING_METADATA, DAPR_PUBSUB_METADATA } from './constants';
+import { DaprBinding, DaprBindingMetadata } from './dapr-binding.decorator';
+import { DaprMetadataAccessor } from './dapr-metadata.accessor';
+import { DaprPubSub, DaprPubSubMetadata } from './dapr-pubsub.decorator';
+import { DaprLoader } from './dapr.loader';
 import { DaprModule } from './dapr.module';
 
-export { DaprBinding, DaprPubSub, DaprModule };
+export {
+  DAPR_BINDING_METADATA,
+  DAPR_PUBSUB_METADATA,
+  DaprMetadataAccessor,
+  DaprBindingMetadata,
+  DaprBinding,
+  DaprPubSubMetadata,
+  DaprPubSub,
+  DaprLoader,
+  DaprModule,
+};
