597a399e10b4726fde0948c1129838d6dba3aaf5
Neil Dobson <neil.dobson@deepbluecompany.com.au>
2022-07-05 08:09:26 +1000

    Fix basics example

--- examples/basics/src/app.controller.spec.ts
+++ examples/basics/src/app.controller.spec.ts
@@ -1,14 +1,20 @@
+import { DaprClient } from '@dapr/dapr';
+import { createMock, DeepMocked } from '@golevelup/ts-jest';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 describe('AppController', () => {
   let appController: AppController;
+  let daprMock: DeepMocked<DaprClient> = createMock<DaprClient>();
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [AppService, {
+        provide: DaprClient,
+        useValue: daprMock
+      }],
     }).compile();
 
     appController = app.get<AppController>(AppController);
